b73fe9b3a6bffb0770c63e15282c92e161da8444
Tyler Veltkamp <veltkamptyler@gmail.com>
2023-04-03 14:57:33 -0500

    first commit of first C# program

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld {
+    class StarsMain {
+        public static void Main(string[] args) {
+            Console.Write("Enter the number of rows: ");
+            string userInput = Console.ReadLine();
+            int numRows = int.Parse(userInput);
+
+            for (int i = 0; i < numRows; i++) {
+
+                for (int j = numRows - i; j > 0; j--) {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= (i*2) + 1; k++) {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
+
